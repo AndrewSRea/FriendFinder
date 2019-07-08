1c2646f2d1f1008fa456cc91b4aff5a865349e91
Virtual Drew <andrew.s.rea@gmail.com>
2019-07-08 13:03:46 -0700

    Most coding is done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,39 @@
-test pattern
+var friends = [
+  {
+    name: "Tom Hardy",
+    photo: "https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/2048x2730/2048x2730-tom-hardy-esquire-may-cover-promo-11-jpg-4c8bc653.jpg?resize=480:*",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Daisy Ridley",
+    photo: "https://vignette.wikia.nocookie.net/sonypicturesanimation/images/7/74/213345.jpg/revision/latest?cb=20171115164714",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    name: "Benedict Cumberbatch",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Benedict_Cumberbatch_by_Gage_Skidmore.jpg/220px-Benedict_Cumberbatch_by_Gage_Skidmore.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    name: "Scarlett Johansson",
+    photo: "http://www1.pictures.zimbio.com/gi/Scarlett+Johansson+Captain+America+Winter+fiRjv3VVCOcl.jpg",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    name: "Natalie Portman",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Natalie_Portman_Cannes_2015_5_%28cropped%29.jpg/220px-Natalie_Portman_Cannes_2015_5_%28cropped%29.jpg",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    name: "Gary Oldman",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Gary_Oldman_Cannes_2018.jpg/250px-Gary_Oldman_Cannes_2018.jpg",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    name: "Harrison Ford",
+    photo: "https://articlebio.com/uploads/bio/2016/03/09/harrison-ford.jpg",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  },
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,57 +1,64 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friends = require("../data/friends");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+    res.json(friends);
   });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (friendData.length < 10) {
-      friendData.push(req.body);
-      res.json(true);
+    var totalDifference = 0;
+    
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item) {
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
+
+    console.log("Name " + userName);
+    console.log("User score " + userScores);
+
+    var sum = b.reduce((a, b) => a + b, 0);
+
+    console.log("Sum of users score " + sum);
+    console.log("Best match friend diff " + bestMatch.friendDifference);
+    console.log("**********************************");
+
+    for (var i = 0; i < friends.length; i++); {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      console.log("Total Diff " + totalDifference);
+      console.log("Best match friend diff " + bestMatch.friendDifference);
+
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      console.log("Total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("-------------------------> " + totalDifference);
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+      console.log(totalDifference + " Total Difference");
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+    console.log(bestMatch);
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+    friends.push(userData);
+    console.log("New user added");
+    console.log(userData);
+    res.json(bestMatch);
   });
 };
